69fafa7eaf47dc95a6049b5020f8db90cc178242
ZHANG2652136 <104855410+ZHANG2652136@users.noreply.github.com>
2024-01-02 02:49:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+./scripts/feeds install -a
