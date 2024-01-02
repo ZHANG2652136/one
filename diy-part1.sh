5e4cf03531410eccd394f9f9d7deebe9fc694a3a
ZHANG2652136 <104855410+ZHANG2652136@users.noreply.github.com>
2024-01-02 12:07:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,17 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git zerotier https://github.com/immortalwrt/luci/tree/master/applications/luci-app-zerotier' >>feeds.conf.default
+#echo 'src-git homeproxy https://github.com/lxiaya/openwrt-homeproxy.git' >>feeds.conf.default
+#rm -rf target/linux/meson
+
+#svn export https://github.com/lxiaya/openwrt-onecloud/trunk/target/linux/meson target/linux/meson
+
+#svn export -q https://github.com/coolsnowwolf/lede/trunk/target/linux/meson target/linux/meson
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
-./scripts/feeds install -a
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+#echo 'src-git clash https://github.com/lxiaya/onecloud-package' >>feeds.conf.default
+echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/lxiaya/helloworld' >>feeds.conf.default
